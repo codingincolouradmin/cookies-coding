e5716cbf91bfc9cc2e4ac9611d02f4cc7879d04c
tazz111233 <tasneem@codingincolour.org>
2023-10-18 13:33:47 -0400

    added funtionaslity to landing page

---
+++ public/script.js
@@ -0,0 +1,24 @@
+var images = [];
+var time = 3000;
+var i=0;
+
+images[0]="img/elon.png";
+images[1]="img/billy.png";
+images[2]="img/zucc.png";
+
+function changeImg(){
+    document.getElementById("slide").src = images[i];
+    if(i< images.length-1){
+        i++;
+    }
+    else{
+        i = 0;
+
+    }
+
+    setTimeout("changeImg()",time);
+
+}
+
+
+window.onload=changeImg;
