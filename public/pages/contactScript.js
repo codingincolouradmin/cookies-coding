08fc52274b8b9fa8dd6c22b20b951f489f6374db
aleemer <aleemertariq@gmail.com>
2023-10-20 09:41:15 -0700

    Fixed toggle issue.

--- public/pages/contactScript.js
+++ public/pages/contactScript.js
@@ -27,15 +27,16 @@ spanNavLink.forEach(function(span) {
 })
 
 const themeDropdown = document.getElementById('changeThemeDropdown');
-themeDropdown.addEventListener("change", function(event) {
-    event.preventDefault(); // May not be necessary
+themeDropdown.addEventListener("change", function() {
     const selectedOption = themeDropdown.options[themeDropdown.selectedIndex].innerText;
     if (selectedOption === "Dark") {
-        let linkedStylesheet = document.getElementById('styles');
-        linkedStylesheet.setAttribute('href', './contactStylesDark.css')
+        console.log('we are dark');
+        document.body.classList.remove("light-theme");
+        document.body.classList.toggle("dark-theme");
     } else if (selectedOption == "Light") {
-        let linkedStylesheet = document.getElementById('styles');
-    linkedStylesheet.setAttribute('href', './contactStylesLight.css')
+        console.log('we are light');
+        document.body.classList.remove("dark-theme");
+        document.body.classList.toggle("light-theme");
     } else {
         console.log("nothing");
     }
