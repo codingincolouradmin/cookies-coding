23974dabae13d6dbf3cd50808828243d28851ef9
aleemer <aleemertariq@gmail.com>
2023-10-20 10:29:08 -0700

    More changes.

--- public/pages/contactScript.js
+++ public/pages/contactScript.js
@@ -30,16 +30,14 @@ const themeDropdown = document.getElementById('changeThemeDropdown');
 themeDropdown.addEventListener("change", function() {
     const selectedOption = themeDropdown.options[themeDropdown.selectedIndex].innerText;
     if (selectedOption === "Dark") {
-        console.log('we are dark');
+        console.log(document.body.classList);
         document.body.classList.remove("light-theme");
         document.body.classList.toggle("dark-theme");
     } else if (selectedOption == "Light") {
-        console.log('we are light');
         document.body.classList.remove("dark-theme");
         document.body.classList.toggle("light-theme");
     } else {
         console.log("nothing");
     }
 })
-
 
